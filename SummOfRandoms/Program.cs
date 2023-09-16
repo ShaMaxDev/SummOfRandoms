ec4f9088f5fee13fd7ecc7d1934bc646ac504556
ShaMaxDev <maksimbychkov@yahoo.com>
2023-09-16 11:57:50 -0700

    Add project files.

---
+++ SummOfRandoms/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SummOfRandoms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int number = rand.Next(1, 101);
+            int summ = 0;
+
+            for (int i = number; i > 0; i--) 
+            {
+                if (i % 5 == 0 || i % 3 == 0) 
+                {
+                    summ = summ + i;
+                }
+            }
+                    Console.WriteLine(summ); 
+        }
+    }
+}
